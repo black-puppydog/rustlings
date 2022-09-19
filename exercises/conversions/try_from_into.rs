44b1dd22b47a55591f8a83f26c5b0be436020b83
Daan Wynen <black.puppydog@gmx.de>
2022-09-19 08:19:43 +0200

    rest of solutions.
    try_from_into will only work with nightly rust at the moment.

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -4,6 +4,7 @@
 // instead of the target type itself.
 // You can read more about it at https://doc.rust-lang.org/std/convert/trait.TryFrom.html
 // Execute `rustlings hint try_from_into` or use the `hint` watch subcommand for a hint.
+#![feature(array_try_map)]
 
 use std::convert::{TryFrom, TryInto};
 
@@ -23,8 +24,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation
 // and return an Ok result of inner type Color.
 // You need to create an implementation for a tuple of three integers,
@@ -38,6 +37,11 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        let red: u8 = u8::try_from(tuple.0).or(Err(IntoColorError::IntConversion))?;
+        let green: u8 = u8::try_from(tuple.1).or(Err(IntoColorError::IntConversion))?;
+        let blue: u8 = u8::try_from(tuple.2).or(Err(IntoColorError::IntConversion))?;
+
+        Ok(Color { red, green, blue })
     }
 }
 
@@ -45,6 +49,12 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let colors = arr.try_map(u8::try_from).or(Err(IntoColorError::IntConversion))?;
+        Ok(Color {
+            red: colors[0], 
+            green: colors[1], 
+            blue: colors[2], 
+        })
     }
 }
 
@@ -52,6 +62,8 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        let array = <&[i16; 3]>::try_from(slice).or(Err(IntoColorError::BadLen))?;
+        Color::try_from(*array)
     }
 }
 
